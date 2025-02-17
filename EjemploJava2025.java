ba5b1edbdba85419727c24d55c69b2c999d5c807
PatriciaSMA1 <patrigoex@gmail.com>
2025-02-17 16:02:11 +0100

    Add files via upload

---
+++ EjemploJava2025.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package EjemploJava2025;
+
+/**
+ *
+ * @author GS
+ */
+public class EjemploJava2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Patricia");
+
+    }
+}
